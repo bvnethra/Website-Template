dc72616b198a45798c9471e6e8f9fd87dbdabc4e
vishalav2005 <vishalav604@gmail.com>
2026-08-27 21:55:34 +0530

    fix: replace incorrectly copied portal files with correct template code for resume-4, 7, and 8

--- templates/resume/resume-8/vite.config.js
+++ templates/resume/resume-8/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/templates/resume/resume-8/",
   plugins: [react()],
 })
-

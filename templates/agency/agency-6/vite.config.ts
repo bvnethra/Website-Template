ee9a0fe6208218a47c0539ed5b8eff0eeb4022b9
vishalav2005 <vishalav604@gmail.com>
2026-08-28 06:23:55 +0530

    fix: configure base path prefix for agency-6 Vite config to resolve assets loading

--- templates/agency/agency-6/vite.config.ts
+++ templates/agency/agency-6/vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/templates/agency/agency-6/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
